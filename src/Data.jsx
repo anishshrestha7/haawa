babff3aaa6c864f5a572f31d8660041941de0b2a
anishshrestha7 <anikazeani7@gmail.com>
2026-02-16 21:09:25 +0545

    Fixed  pathing for subcribe image, tour images,our featured image,hero section images,and group tour ...family tour images

--- src/Data.jsx
+++ src/Data.jsx
@@ -5,41 +5,41 @@ export let popDestination=[
 ]
 
 export let gTours=[
-    {id:1,name:"California Sunset/Twilight Boat Cruise",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"public/image/1.jpg"},
-    {id:2,name:"Pacific Coastal Evening Sailing Tours",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"public/image/2.jpg"},
-    {id:3,name:"Golden Gate Sunset Bay Cruise Voyage",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"public/image/3.jpg"},
-    {id:4,name:"Laguna Beach Twilight Ocean Yacht Ride",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"public/image/4.jpg"},
-    {id:5,name:"Laguna Beach Twilight Ocean Yacht Ride",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"public/image/5.jpg"},
-    {id:6,name:"Monterey Bay Sunset Wildlife Trip Tour",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"public/image/6.jpg"},
+    {id:1,name:"California Sunset/Twilight Boat Cruise",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"/image/1.jpg"},
+    {id:2,name:"Pacific Coastal Evening Sailing Tours",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"/image/2.jpg"},
+    {id:3,name:"Golden Gate Sunset Bay Cruise Voyage",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"/image/3.jpg"},
+    {id:4,name:"Laguna Beach Twilight Ocean Yacht Ride",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"/image/4.jpg"},
+    {id:5,name:"Laguna Beach Twilight Ocean Yacht Ride",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"/image/5.jpg"},
+    {id:6,name:"Monterey Bay Sunset Wildlife Trip Tour",des:"2 days 3 nights",des1:"4-6 guest",price:"$48.25",person:"/ person",button:"Book Now",image:"/image/6.jpg"},
 ]
 
 export let tourPackage=[
-    {id:1,title:"Honeymoon Tour",heading:"Eternal Love Begins Here",des:"2 Country &amp; 15 Location",button:"View This Trip",image:"/public/tour/1.jpg"},
-    {id:2,title:"Family Tour",heading:"Family Tour Deal",des:"2 Country &amp; 10 Location",button:"View This Trip",image:"/public/tour/2.jpg"},
-    {id:3,title:"Adventure Tour",heading:"Adventure Fun",des:"4 Country &amp; 25 Location",button:"View This Trip",image:"/public/tour/3.jpg"},
-    {id:4,title:"Luxury Escape",heading:"Enjoy 10% Off",des:"3 Country &amp; 12 Location",button:"View This Trip",image:"/public/tour/4.jpg"},
+    {id:1,title:"Honeymoon Tour",heading:"Eternal Love Begins Here",des:"2 Country &amp; 15 Location",button:"View This Trip",image:"/tour/1.jpg"},
+    {id:2,title:"Family Tour",heading:"Family Tour Deal",des:"2 Country &amp; 10 Location",button:"View This Trip",image:"/tour/2.jpg"},
+    {id:3,title:"Adventure Tour",heading:"Adventure Fun",des:"4 Country &amp; 25 Location",button:"View This Trip",image:"/tour/3.jpg"},
+    {id:4,title:"Luxury Escape",heading:"Enjoy 10% Off",des:"3 Country &amp; 12 Location",button:"View This Trip",image:"/tour/4.jpg"},
 ]
 
 export let desTours=[
-    {id:1,name:"Venice",des:"306 Tours",image:"/public/description/sm-1.jpg"},
-    {id:2,name:"Bali",des:"256 Tours",image:"/public/description/sm-2.jpg"},
-    {id:3,name:"James Bond Island",des:"256 Tours",image:"/public/description/sm-3.jpg"},
-    {id:4,name:"Phuket",des:"256 Tours",image:"/public/description/sm-4.jpg"},
-    {id:5,name:"Chiang Mai",des:"226 Tours",image:"/public/description/sm-5.jpg"},
-    {id:6,name:"Bana Hills",des:"126 Tours",image:"/public/description/sm-6.jpg"},
-    {id:7,name:"Venice",des:"306 Tours",image:"/public/description/sm-1.jpg"},
-    {id:8,name:"Bali",des:"256 Tours",image:"/public/description/sm-2.jpg"},
+    {id:1,name:"Venice",des:"306 Tours",image:"/description/sm-1.jpg"},
+    {id:2,name:"Bali",des:"256 Tours",image:"/description/sm-2.jpg"},
+    {id:3,name:"James Bond Island",des:"256 Tours",image:"/description/sm-3.jpg"},
+    {id:4,name:"Phuket",des:"256 Tours",image:"/description/sm-4.jpg"},
+    {id:5,name:"Chiang Mai",des:"226 Tours",image:"/description/sm-5.jpg"},
+    {id:6,name:"Bana Hills",des:"126 Tours",image:"/description/sm-6.jpg"},
+    {id:7,name:"Venice",des:"306 Tours",image:"/description/sm-1.jpg"},
+    {id:8,name:"Bali",des:"256 Tours",image:"/description/sm-2.jpg"},
 
 ]
 export let desFamily=[
-    {id:1,name:"Phuket",des:"256 Tours",image:"/public/description/sm-4.jpg"},
-    {id:2,name:"Chiang Mai",des:"226 Tours",image:"/public/description/sm-5.jpg"},
-    {id:3,name:"Bana Hills",des:"126 Tours",image:"/public/description/sm-6.jpg"},
-    {id:4,name:"Venice",des:"306 Tours",image:"/public/description/sm-1.jpg"},
-    {id:5,name:"Bali",des:"256 Tours",image:"/public/description/sm-2.jpg"},
-    {id:6,name:"James Bond Island",des:"256 Tours",image:"/public/description/sm-3.jpg"},
-    {id:7,name:"Phuket",des:"256 Tours",image:"/public/description/sm-4.jpg"},
-    {id:8,name:"Chiang Mai",des:"226 Tours",image:"/public/description/sm-5.jpg"},
+    {id:1,name:"Phuket",des:"256 Tours",image:"/description/sm-4.jpg"},
+    {id:2,name:"Chiang Mai",des:"226 Tours",image:"/description/sm-5.jpg"},
+    {id:3,name:"Bana Hills",des:"126 Tours",image:"/description/sm-6.jpg"},
+    {id:4,name:"Venice",des:"306 Tours",image:"/description/sm-1.jpg"},
+    {id:5,name:"Bali",des:"256 Tours",image:"/description/sm-2.jpg"},
+    {id:6,name:"James Bond Island",des:"256 Tours",image:"/description/sm-3.jpg"},
+    {id:7,name:"Phuket",des:"256 Tours",image:"/description/sm-4.jpg"},
+    {id:8,name:"Chiang Mai",des:"226 Tours",image:"/description/sm-5.jpg"},
     
 ]
 
